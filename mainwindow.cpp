682404841ff26f445ad46a1a60128ecb6746d6a0
JTAO <jerky168@163.com>
2016-11-06 21:55:31 +0800

    finish the view function

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,14 +7,19 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    //add the toobar and dock windows to menu view
+    ui->menuView->addAction(ui->mainToolBar->toggleViewAction());
+    ui->menuView->addAction(ui->dockWidgetImage->toggleViewAction());
+    ui->menuView->addAction(ui->dockWidgetDefect->toggleViewAction());
+
     //make the 2D view and 3D view exclusive
     QActionGroup* actionGroup = new QActionGroup(this);
     actionGroup->addAction(ui->action2DView);
     actionGroup->addAction(ui->action3DView);
 
     //switch 2D view and 3D view
-    connect(ui->action2DView, &QAction::triggered, [this](bool checked) {ui->stackedWidget->setCurrentIndex(0);});
-    connect(ui->action3DView, &QAction::triggered, [this](bool checked) {ui->stackedWidget->setCurrentIndex(1);});
+    connect(ui->action2DView, &QAction::triggered, [this](bool checked) {if (checked) ui->stackedWidget->setCurrentIndex(0);});
+    connect(ui->action3DView, &QAction::triggered, [this](bool checked) {if (checked) ui->stackedWidget->setCurrentIndex(1);});
 
 }
 
