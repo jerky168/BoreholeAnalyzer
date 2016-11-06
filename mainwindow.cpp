e886490e78f0208fc0738d9240c36899d5a07ddb
JTAO <jerky168@163.com>
2016-11-06 21:10:26 +0800

    make the 2D and 3D button functional

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,9 +7,15 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    //make the 2D view and 3D view exclusive
     QActionGroup* actionGroup = new QActionGroup(this);
     actionGroup->addAction(ui->action2DView);
     actionGroup->addAction(ui->action3DView);
+
+    //switch 2D view and 3D view
+    connect(ui->action2DView, &QAction::triggered, [this](bool checked) {ui->stackedWidget->setCurrentIndex(0);});
+    connect(ui->action3DView, &QAction::triggered, [this](bool checked) {ui->stackedWidget->setCurrentIndex(1);});
+
 }
 
 MainWindow::~MainWindow()
