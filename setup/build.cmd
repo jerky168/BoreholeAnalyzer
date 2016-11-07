4350c6c8ed1b07faff75670c99f33dcfc7ac4ea1
JTAO <jerky168@163.com>
2016-11-07 12:08:04 +0800

    move the build script to setup directory

--- setup/build.cmd
+++ setup/build.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+echo Setting up a Qt environment...
+echo -- QMAKESPEC set to "win32-msvc2015"
+
 set QMAKESPEC=win32-msvc2015
 if exist env.cmd (
 	echo "environment set, using env.cmd."
@@ -11,8 +14,7 @@ if exist env.cmd (
 	set VS_DIR="C:\Program Files (x86)\Microsoft Visual Studio 14.0"
 )
 
-echo Setting up a Qt environment...
-echo -- QMAKESPEC set to "win32-msvc2015"
+
 echo -- QT_DIR set to %QT_DIR%
 echo -- QT_INSTALLER_DIR set to %QT_INSTALLER_DIR%
 echo -- VS_DIR set to %VS_DIR%
@@ -23,10 +25,9 @@ echo -- Added %QT_DIR%\bin;%QT_INSTALLER_DIR%\bin; to PATH
 
 call %VS_DIR%\VC\vcvarsall.bat x86
 
-set SRC=%cd%
+set SRC=%cd%\..
 echo --SRC set to %SRC%
 
-cd ..
 set CURRENT_TIME="%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%.%time:~3,2%.%time:~6,2%"
 echo --CURRENT_TIME set to %CURRENT_TIME%
 mkdir BoreHoleAnalyzer_%CURRENT_TIME%
@@ -39,14 +40,9 @@ qmake %SRC%\BoreholeAnalyzer.pro
 nmake release /nologo
 
 copy release\BoreholeAnalyzer.exe .\dist\packages\com.ylink.idt\data
-del Makefile*
-rd /q /s build debug release
-
 
 cd dist\packages\com.ylink.idt\data
 windeployqt BoreholeAnalyzer.exe
 
 cd ..\..\..\..
 binarycreator.exe -c dist\config\config.xml -p dist\packages "IDT(M) Installer.exe" -v
-rd /q /s dist
-
