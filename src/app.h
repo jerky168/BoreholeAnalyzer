b386cfa64523fb7207c0a0374e4beffae469c129
JTAO <jerky168@163.com>
2017-01-06 11:29:39 +0800

    修改正式发布版本号

--- src/app.h
+++ src/app.h
@@ -3,9 +3,9 @@
 #include <QObject>
 #include <QString>
 
-#define     Major_Version_Number    "0"
-#define     Minor_Version_Number    "3"
-#define     Data_Version_Number     "0102"
+#define     Major_Version_Number    "1"
+#define     Minor_Version_Number    "0"
+#define     Data_Version_Number     "103"
 
 #define     App_Name        QObject::tr("BoreholeAnalyzer")
 #define     App_Version     Major_Version_Number + "." + Minor_Version_Number + "." + Data_Version_Number
