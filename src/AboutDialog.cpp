9a004c0a7f5e81041063480555a8ea468bf86c9f
JTAO <jerky168@163.com>
2017-01-18 18:21:02 +0800

    更新版本号

--- src/AboutDialog.cpp
+++ src/AboutDialog.cpp
@@ -7,15 +7,8 @@ AboutDialog::AboutDialog(QWidget *parent) :
 {
     ui->setupUi(this);
     setWindowTitle(App_Name_CN);
-    ui->contentLabel->setText(App_Name_CN + "\nv" + App_Version);
+    ui->contentLabel->setText(App_Name_CN + "\nV" + App_Version);
     ui->buttonBox->button(QDialogButtonBox::Ok)->setText(tr("Ok"));
-
-    QFile file("changelog");
-    if (file.exists())
-    {
-        file.open(QIODevice::ReadOnly);
-        ui->plainTextEdit->setPlainText(QString(file.readAll()));
-    }
 }
 
 AboutDialog::~AboutDialog()
--- src/app.h
+++ src/app.h
@@ -5,14 +5,13 @@
 #include <QDir>
 
 #define     Major_Version_Number    "1"
-#define     Minor_Version_Number    "1"
-#define     Revision_Number         "2"
-#define     Build_Number            "20170109"
+#define     Minor_Version_Number    "2"
+#define     Revision_Number         "0"
 
 #define     App_Name_CN         QObject::tr("BoreholeAnalyzer")
 #define     App_Name_EN         "BoreholeAnalyzer"
 #define     App_Version         QString(Major_Version_Number) + "." + Minor_Version_Number \
-                                + "." + Revision_Number + " build-" + Build_Number
+                                + "." + Revision_Number + " build-" + __DATE__
 #define     Company_Name_CN     QObject::tr("Y-Link")
 #define     Company_Name_EN     "Y-Link"
 #define     Company_Url         "http://www.y-link.cn/"
