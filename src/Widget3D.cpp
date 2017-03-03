1efa6e471017910f7213e1dbb92ced51938edeb5
Jayson Jiang <suisiyuan615@qq.com>
2017-03-03 10:02:19 +0800

    上传修改所有bug后的版本。

---
+++ src/Widget3D.cpp
@@ -0,0 +1,6 @@
+#include "Widget3D.h"
+
+Widget3D::Widget3D()
+{
+
+}
---
+++ src/Widget3D.h
@@ -0,0 +1,11 @@
+#ifndef WIDGET3D_H
+#define WIDGET3D_H
+
+
+class Widget3D : public QOpenGLWidget
+{
+public:
+    Widget3D();
+};
+
+#endif // WIDGET3D_H
